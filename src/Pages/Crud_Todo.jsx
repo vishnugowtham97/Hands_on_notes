e51abc8c7211f69bd7b281f0ed525982d30dece0
vishnugowtham97 <vishnugowtham97@gmail.com>
2024-01-06 08:24:36 +0530

    form vaidation added

--- src/Pages/Crud_Todo.jsx
+++ src/Pages/Crud_Todo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Addmember from "../Crud/Addmember";
 import Todo_form from "../Todo/Todo_form";
+import Form from "../Form_Validation/Form";
 
 const Crud_Todo = () => {
   return (
     <div>
       <Addmember />
       <Todo_form />
+      <Form />
     </div>
   );
 };
